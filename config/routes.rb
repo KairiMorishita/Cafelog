ced6fe25298facf5212169aea9cc938a8c869050
Kairi Morishita <k.moricchifc73@gmail.com>
2020-03-15 05:49:36 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-   get 'users/new'
-
+    get 'sessions/new'
+    
     root 'cafes#index'
+    get  '/signup',   to: 'users#new'
+    get    '/login',   to: 'sessions#new'
+    post   '/login',   to: 'sessions#create'
+    delete '/logout',  to: 'sessions#destroy'
+
     resources :cafes
     resources :users
 end
