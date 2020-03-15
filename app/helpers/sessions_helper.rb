ebd8e075fe5d2fdd99aa2588580851069c70a0d5
Kairi Morishita <k.moricchifc73@gmail.com>
2020-03-15 21:27:41 +0000

    Implemenent advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,15 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  
+  
   # 永続セッションとしてユーザーを記憶する
   def remember(user)
     user.remember
@@ -17,13 +26,20 @@ module SessionsHelper
     user == current_user
   end
   
-  # 現在ログイン中のユーザーを返す (いる場合)
+   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
-  
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
@@ -33,6 +49,21 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+    # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   
   # 記憶したURL (もしくはデフォルト値) にリダイレクト
   def redirect_back_or(default)
