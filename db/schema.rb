b5b0827a439d6e9a630a9a0b9f597e6409e33353
Kairi Morishita <k.moricchifc73@gmail.com>
2020-03-20 03:20:19 +0000

    Add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200315223758) do
+ActiveRecord::Schema.define(version: 20200320022053) do
 
   create_table "cafes", force: :cascade do |t|
     t.string "name"
@@ -32,8 +32,10 @@ ActiveRecord::Schema.define(version: 20200315223758) do
     t.string "password_digest"
     t.boolean "admin", default: false
     t.string "remember_digest"
+    t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
