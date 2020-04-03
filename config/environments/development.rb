285ac343b56ed48bcca8a0418cc9e5694f1bb6a4
Kairi Morishita <k.moricchifc73@gmail.com>
2020-04-03 15:38:30 +0000

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
   host = 'cdb75f88f7674b61bd0c7dc31f5019f2.vfs.cloud9.ap-northeast-1.amazonaws.com/' # ここをコピペすると失敗します。自分の環境に合わせてください。
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
